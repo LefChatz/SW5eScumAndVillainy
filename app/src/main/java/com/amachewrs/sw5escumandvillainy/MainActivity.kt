b5b2dcfb6cebe811e0f1840ec94b36d87d2c89b9
LefChatz <146983163+LefChatz@users.noreply.github.com>
2025-01-09 22:49:45 +0200

    errorButton

--- app/src/main/java/com/amachewrs/sw5escumandvillainy/MainActivity.kt
+++ app/src/main/java/com/amachewrs/sw5escumandvillainy/MainActivity.kt
@@ -1,32 +1,29 @@
 package com.amachewrs.sw5escumandvillainy
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.amachewrs.sw5escumandvillainy.ui.theme.SW5eScumAndVillainyTheme
 
+
 class MainActivity : ComponentActivity() {
+
+    private val errorlist = listOf(1,2,3,4,5,6,7,8,9,10)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            SW5eScumAndVillainyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.gay)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            for(i in 1..20){
+                errorlist[i].plus(2)
             }
         }
+
     }
 }
 
